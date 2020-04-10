460d7571378c7733f72755a5ff0431230fbb3a85
eraj2587 <e.raj2587@gmail.com>
2020-04-11 02:12:08 +0530

    Add project files.

---
+++ MeterReaderWeb/Services/MeterService.cs
@@ -0,0 +1,60 @@
+﻿using Grpc.Core;
+using MeterReaderWeb.Data;
+using MeterReaderWeb.Data.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeterReaderWeb.Services
+{
+    public class MeterService : MeterReadingService.MeterReadingServiceBase
+    {
+        private readonly ILogger<MeterService> logger;
+        private readonly IReadingRepository readingRepository;
+
+        public MeterService(ILogger<MeterService> logger, IReadingRepository readingRepository)
+        {
+            this.logger = logger;
+            this.readingRepository = readingRepository;
+        }
+
+        public async override Task<StatusMessage> AddReading(ReadingPacket request, ServerCallContext context)
+        {
+            var result = new StatusMessage()
+            {
+                Success = ReadingStatus.Failure
+            };
+
+            try
+            {
+                if (request.Successful == ReadingStatus.Success)
+                {
+                    foreach (var reading in request.Readings)
+                    {
+                        var read = new MeterReading
+                        {
+                            CustomerId = reading.CustomerId,
+                            Value = reading.ReadingValue,
+                            ReadingDate = reading.ReadingTime.ToDateTime()
+                        };
+                    readingRepository.AddEntity(read);
+                    }
+                   if(await readingRepository.SaveAllAsync())
+                    {
+                        result.Success = ReadingStatus.Success;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Message = "Exception throwing during process";
+                logger.LogError($"Error occurred while processing : {ex}");
+            }
+
+            return result;
+            
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+Rebuild the project
+Change the connectionstring in the web project as per database
+Create database in your database server
+Run update-database command
